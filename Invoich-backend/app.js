a01bf549120c20ffa5b149b7a7465bb94ca65a72
monil <patelmonil7908@gmail.com>
2025-05-31 10:12:53 +0530

    Update server port to 8001 and upgrade dotenv to version 16.5.0

--- Invoich-backend/app.js
+++ Invoich-backend/app.js
@@ -54,7 +54,7 @@ app.use('/api/usercomplain', usercomplain);
 
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8001;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
